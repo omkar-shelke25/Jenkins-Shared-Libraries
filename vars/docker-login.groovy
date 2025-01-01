de2a14c0c7e946f5cb50ca990295532249161f3a
Omkar Shelke <85251955+omkar-shelke25@users.noreply.github.com>
2025-01-01 18:22:25 +0530

    Create docker-login.groovy

---
+++ vars/docker-login.groovy
@@ -0,0 +1,2 @@
+# Jenkins-Shared-Libraries
+This repository contains reusable Groovy scripts and functions for Jenkins pipelines. It provides a centralized way to manage and share common pipeline logic, ensuring consistency and reducing duplication across multiple Jenkins jobs.
