9cd90e38b35ca5699afef8efb19c630b9851f159
Omkar Shelke <85251955+omkar-shelke25@users.noreply.github.com>
2025-03-05 18:10:07 +0530

    Update trivy_fs_scan.groovy

--- vars/trivy_fs_scan.groovy
+++ vars/trivy_fs_scan.groovy
@@ -1,7 +1,7 @@
 def call() {
     try {
         sh 'trivy fs --exit-code 1 --severity CRITICAL .'
-        # sh 'trivy fs --exit-code 1 --severity HIGH,CRITICAL .'
+       
     } catch (Exception e) {
         echo "Vulnerabilities found! Check the Trivy report."
         currentBuild.result = 'FAILURE'
