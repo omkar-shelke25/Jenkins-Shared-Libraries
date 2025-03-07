83b6e817924dd5809ddcbc635af09d443b7bf4e3
Omkar Shelke <85251955+omkar-shelke25@users.noreply.github.com>
2025-03-07 11:54:52 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -6,8 +6,7 @@ def call(String imageName, String tag) {
         // Login to Docker using the credentials
         sh "docker login -u ${USERNAME} -p ${PASSWORD}"
         
-        // Tag the Docker image with the specified username, image name, and tag
-        sh "docker tag ${imageName} ${USERNAME}/${imageName}:${tag}"
+        
         
         // Push the tagged Docker image to Docker Hub or your Artifactory
         sh "docker push ${USERNAME}/${imageName}:${tag}"
