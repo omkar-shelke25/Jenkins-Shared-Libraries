31d727d6b501bad4d9cd89266abfa90a2900e6f6
Omkar Shelke <85251955+omkar-shelke25@users.noreply.github.com>
2025-03-07 11:55:37 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,10 @@
-
-def call(String imageName){
-    sh "sudo docker build -t ${imageName} ." 
+def call(String imageName, String tag) {
+    echo "Docker Login and Push The Image In Artifactory"
+    
+    // Use withCredentials to securely fetch credentials from Jenkins' credentials store
+    withCredentials([usernamePassword(credentialsId: 'docker', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+        
+        // Push the tagged Docker image to Docker Hub or your Artifactory
+        sh "docker build -t ${USERNAME}/${imageName}:${tag}"
+    }
 }
